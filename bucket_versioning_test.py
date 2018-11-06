613edbcbe5755c3e6efb8f39938785eb3d1c0eb4
Enming.Zhang <zvampirem77@gmail.com>
2018-11-06 01:10:48 -0500

    add enable and suspend bucket versioning test cases
    Signed-off-by: Enming.Zhang <zvampirem77@gmail.com>

--- bucket_versioning_test.py
+++ bucket_versioning_test.py
@@ -8,6 +8,17 @@ from botocore.client import Config
 def get_bucket_versioning(client, bucket):
     return client.get_bucket_versioning(Bucket = bucket)
 
+def put_bucket_versioning(client, bucket):
+    return client.put_bucket_versioning(Bucket = bucket,
+                                        VersioningConfiguration = {
+                                            'Status': 'Enabled'})
+
+def suspend_bucket_versioning(client, bucket):
+    return client.put_bucket_versioning(Bucket = bucket,
+                                        VersioningConfiguration = {
+                                            'Status': 'Suspended'})
+
+
 
 def main():
     s3client = boto3.client('s3', endpoint_url = 'http://192.168.2.17:8080',
@@ -15,10 +26,18 @@ def main():
                             aws_secret_access_key = 'em_test1',
                             config = Config(signature_version = 's3v4'))
 
-    opts, args = getopt.getopt(sys.argv[1:], 'g:', ['get='])
+    opts, args = getopt.getopt(sys.argv[1:], 'g:p:s:', ['get=', 'put=', 'susp='])
     if opts:
         for o, a in opts:
             if o in ('-g', '--get'):
+                res = get_bucket_versioning(s3client, a)
+                print res
+            elif o in ('-p', '--put'):
+                put_bucket_versioning(s3client, a)
+                res = get_bucket_versioning(s3client, a)
+                print res
+            elif o in ('-s', '--susp'):
+                suspend_bucket_versioning(s3client, a)
                 res = get_bucket_versioning(s3client, a)
                 print res
     else:
